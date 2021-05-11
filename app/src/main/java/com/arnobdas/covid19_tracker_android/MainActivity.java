cbc0b8e4445b9b9114dd92ad7258f1de220941e7
arnob-das <contact.arnobdas@gmail.com>
2021-05-11 15:50:22 +0600

    load url done

--- app/src/main/java/com/arnobdas/covid19_tracker_android/MainActivity.java
+++ app/src/main/java/com/arnobdas/covid19_tracker_android/MainActivity.java
@@ -3,12 +3,19 @@ package com.arnobdas.covid19_tracker_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView mywebview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mywebview = (WebView) findViewById(R.id.WebViewId);
+        mywebview.loadUrl("https://covid19-tracker-arnob.netlify.app/");
+
     }
 }
